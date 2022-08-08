d4a362cf6222593c0b5595a24f00f5f2fefdfe7a
Iain Lowe <iain.lowe@gmail.com>
2022-08-08 03:40:45 -0400

    add version and commit to output

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
 package main
 import "fmt"
-func main() { fmt.Println("Hello, world! I am super cheerful! And I mean it even more!") }
+var(
+version string
+commit string)
+
+func main() { fmt.Println("Hello, world! I am super cheerful! And I mean it even more!\n",version,"\n",commit) }
